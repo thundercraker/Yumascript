22bb34e5889e04c5feadd1725541549d060b0819
Yumashish Subba <yumashish@gmail.com>
2015-10-19 13:32:08 +0700

    Thinkpad Working Version

---
+++ Yumascript-Package/Linker/YSLinker.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+public class YSLinker
+{
+	public static string CONSOLE_TAG = "[Linker]";
+	public static bool DEBUG = false;
+	public static bool VERBOSE = false;
+
+	string current_resource;
+
+	public struct Resource {
+		public string Name;
+		public string Content;
+	}
+
+	public YSLinker ()
+	{
+		current_resource = "";
+	}
+
+	public Resource[] LoadResources(string[] resource_links)
+	{
+		List<Resource> Resources = new List<Resource> ();
+		foreach (string resource_link in resource_links) {
+			Resources.Add (GetRawFromResourceLocation (resource_link));
+		}
+		return Resources.ToArray ();
+	}
+
+	public Resource GetRawFromResourceLocation(string ResourceLocation)
+	{
+		current_resource = ResourceLocation;
+
+		//simple implementation
+		try
+		{   
+			string raw = "";
+			using (StreamReader sr = new StreamReader(ResourceLocation + ".ys"))
+			{
+				raw = sr.ReadToEnd();
+			}
+			Resource R;
+			R.Name = ResourceLocation;
+			R.Content = raw;
+			return R;
+		}
+		catch (IOException e)
+		{
+			Error ("File could not be read");
+		}
+		return new Resource();
+	}
+
+	void Verbose(String s)
+	{
+		if(VERBOSE)
+			Console.WriteLine (String.Format(CONSOLE_TAG + "[Verbose for ({0})] {1} ", current_resource, s));
+	}
+
+	void Debug(String s)
+	{
+		if(DEBUG)
+			Console.WriteLine (String.Format(CONSOLE_TAG + "[Debug for ({0})] {1} ", current_resource, s));
+	}
+
+	void Error(String s)
+	{
+		Console.WriteLine (String.Format(CONSOLE_TAG + "[Error for ({0})] {1} ", current_resource, s));
+		throw new LinkerException ("Linker Exception");
+	}
+
+	public class LinkerException : Exception
+	{
+		public LinkerException(string msg) : base(msg) {
+		}
+	}
+}
+
---
+++ Yumascript-Package/Yumascript.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+public class Yumascript
+{
+	YSStateModule State;
+
+	public Yumascript (bool DEBUG_MODE)
+	{
+		YSLexer.DEBUG 		= DEBUG_MODE;
+		YSRDParser.DEBUG	= DEBUG_MODE;
+		YSInterpreter.DEBUG = DEBUG_MODE;
+		YSStateModule.DEBUG	= DEBUG_MODE;
+		State =  new YSStateModule ();
+	}
+
+	public void Run(string ProgramPath)
+	{
+		try
+		{   
+			string raw;
+			using (StreamReader sr = new StreamReader(ProgramPath))
+			{
+				raw = sr.ReadToEnd();
+			}
+			bool status = Yumascript.LPIProcess(ref State, raw);
+			Console.WriteLine("Program has finished with status " + status);
+		}
+		catch (IOException e)
+		{
+			Console.WriteLine("The file could not be read:");
+			Console.WriteLine(e.Message);
+		}
+	}
+
+	public static bool LPIProcess(ref YSStateModule state, string raw)
+	{
+		bool result = false;
+		YSLexer lexer = new YSLexer(raw);
+		YSRDParser parser = new YSRDParser (lexer.GetTokenList());
+		if(parser.Parse() <= YSInterpreter.ERR_ACCEPT){
+			YSInterpreter interpreter = new YSInterpreter (parser.PopLast());
+			result = interpreter.Interpret(ref state);
+		} else
+			Console.WriteLine("Interpreter not started, parsing was aborted");
+		return result;
+	}
+}
+
